06e2751d002fd4a85d4651fa96f54fa86ea7df59
astasiempre <ero1494651@gmail.com>
2023-09-20 12:07:52 +0300

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+
+  handleFeedback = (type) => {
+    this.setState((prevState) => ({
+      [type]: prevState[type] += 1,
+    }));
+  };
+
+  render() {
+    const {good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button onClick={()=>{this.handleFeedback('good')}}>Good</button>
+        <button onClick={()=>{this.handleFeedback('neutral')}}>Neutral</button>
+        <button onClick={()=>{this.handleFeedback('bad')}}>Bad</button>
+
+        <h2>Statistic</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
